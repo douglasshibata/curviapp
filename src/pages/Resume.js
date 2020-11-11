5b68f222aeefadf19605b57733c33180c5ff2231
Douglas Shibata <douglas.shibata11@gmail.com>
2020-11-11 17:19:42 -0300

    Adicionando melhorias no pdf

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,28 +4,26 @@ import '../App.css';
 import api from '../services/api';
 import { Form } from './style';
 import { useParams } from 'react-router-dom';
-import { Page, Text, View, Document, StyleSheet, PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet, PDFDownloadLink } from '@react-pdf/renderer';
+import Header from './components/Header';
+import Experience from './components/Experience';
+import Education from './components/Education';
+import ReactLoading from 'react-loading';
 
 // Create styles
 const styles = StyleSheet.create({
   page: {
     padding: 30,
   },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    '@media max-width: 400': {
+      flexDirection: 'column',
+    },
+  },
   image: {
     marginBottom: 10,
-  },
-  leftColumn: {
-    flexDirection: 'column',
-    width: 170,
-    paddingTop: 30,
-    paddingRight: 15,
-    '@media max-width: 400': {
-      width: '100%',
-      paddingRight: 0,
-    },
-    '@media orientation: landscape': {
-      width: 200,
-    },
   },
   footer: {
     fontSize: 12,
@@ -39,44 +37,18 @@ const styles = StyleSheet.create({
       marginTop: 10,
     },
   },
-  container: {
-    flexDirection: 'row',
-    borderBottomWidth: 2,
-    borderBottomColor: '#112131',
-    borderBottomStyle: 'solid',
-    alignItems: 'stretch',
-  },
-  detailColumn: {
-    flexDirection: 'column',
-    flexGrow: 9,
-    textTransform: 'uppercase',
-  },
-  linkColumn: {
-    flexDirection: 'column',
-    flexGrow: 2,
-    alignSelf: 'flex-end',
-    justifySelf: 'flex-end',
-  },
-  name: {
-    fontSize: 24,
-  },
-  link: {
-    fontSize: 10,
-    color: 'black',
-    textDecoration: 'none',
-    alignSelf: 'flex-end',
-    justifySelf: 'flex-end',
-  },
 });
 
 function Resume() {
   const [user, setUser] = useState([])
+  const [show, setHide] = useState(false)
   let { id } = useParams()
   useEffect(() => {
     async function getUser() {
       try {
         const response = await api.get(`/user/${id}`)
         setUser(response.data)
+        setHide(true)
       } catch (error) {
         console.log(error);
       }
@@ -89,6 +61,24 @@ function Resume() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {show &&<PDFDownloadLink fileName={`${user.name}.pdf`} document={
+            <Document>
+              <Page size="A4" style={styles.page}>
+                <Header user={user} />
+                    <Education user={user} />
+                    {/* <Skills /> */}
+                  <Experience user={user} />
+                <Text style={styles.footer}>{user.name} - Candidato(a) ideal para a sua Empresa - Resume Made by Curvi</Text>
+              </Page>
+            </Document>
+        }>
+           {({ blob, url, loading, error }) =>
+          loading ? <ReactLoading type='bars' color="black" height={'5%'} width={'100%'} />
+          : <button type='button'>Baixar o Currículo</button> 
+          
+        }
+        </PDFDownloadLink>
+        }
         <Form>
           <div className="content">
             <h1>Dados Pessoais</h1>
@@ -102,23 +92,25 @@ function Resume() {
             <h2>Objetivo</h2>
             <p> {user.goal} </p>
           </div>
+          <div className="content">
+            <h2>Experiencia</h2>
+            <p> Cargo: {user.companyOccupation} </p>
+            <p> Período: {user.companyStart} - {user.companyEnd} </p>
+            <p>{user.companyName}</p>
+            <p> {user.companyDescription}</p>
+          </div>
+          <div className="content">
+            <h2>Curso</h2>
+            <p>{user.courseName}</p>
+            <p>{user.courseSchool}</p>
+            <p>{user.courseStartYear} - {user.courseEndYear}</p>
+          </div>
+          <div className="content">
+            <h2>Formação Complementar</h2>
+            <p>{user.courses}</p>
+          </div>
         </Form>
       </header>
-      <PDFViewer>
-        <Document>
-          <Page size="A4" style={styles.page}>
-            <View style={styles.container}>
-              <View style={styles.detailColumn}>
-                <Text style={styles.name}>{user.name}</Text>
-              </View>
-              <View style={styles.linkColumn}>
-                <Text style={styles.link}>{user.email}</Text>
-              </View>
-            </View>
-            <Text style={styles.footer}>O melhor Candidato</Text>
-          </Page>
-        </Document>
-        </PDFViewer>
     </div>
   );
 }
