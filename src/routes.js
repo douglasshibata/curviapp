e56c6b71b1a517b1a677991d26235c198694d873
Douglas Shibata <douglas.shibata11@gmail.com>
2020-11-18 10:09:58 -0300

    Removendo rotas não utilizadas

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Switch} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-// import Resume from "./pages/Resume";
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={LandingPage} />
-      {/* <Route path="/:id" component={Resume} /> */}
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
   </BrowserRouter>
