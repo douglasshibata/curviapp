8256127cec123a95d6b96bb515647f8f85a68c4f
Douglas Shibata <douglas.shibata11@gmail.com>
2020-11-20 18:50:04 -0300

    Mudando página incial

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,102 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import '../App.css';
-import api from '../services/api';
-import { Form } from './style';
-import ReactLoading from 'react-loading';
 import logo from '../assets/logo.gif';
-import BasicResume from './BasicResume/BasicResume';
-import JuniorResume from './JuniorResume/JuniorResume';
-import SeniorResume from './SeniorResume/SeniorResume';
-import PlenoResume from './PlenoResume/PlenoResume';
+import ChatBot from './Chatbot/Chatbot';
+import { Box } from './style';
 
 function LandingPage() {
-  const [user, setUser] = useState([])
-  const [show, setHide] = useState(false)
-  const [email, setEmail] = useState('')
-  const [loading, setLoading] = useState(false)
-  useEffect(() => {
-    async function getUser() {
-      try {
-        const response = await api.get(`/api/user`, { headers: { email: email } })
-        setUser(response.data)
-        setHide(true)
-        setLoading(true)
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getUser();
-  }, [email])
-  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Bem vindo a Curvi</p>
-        <Form>
-          <div className="content">
-            <p><label>Confirme o seu Email para baixar o Currículo</label></p>
-            <input type="email" name="email" onChange={e => setEmail(e.target.value)} id="email" />
-          </div>
-        </Form>
-        {loading ? <>
-          {show &&
-            (user.area_level === 'Estagiário(a)' || user.area_level === 'Jovem aprendiz') ? <BasicResume user={user} />
-            :
-            user.area_level === 'Júnior' ? <JuniorResume user={user} />
-              :
-              user.area_level === 'Sênior' ? <SeniorResume user={user} />
-                :
-                user.area_level === 'Pleno' ? <PlenoResume user={user} />
-                  :
-                  <></>}
-          <Form>
-            <div className="content">
-              <h1>Dados Pessoais</h1>
-              <p>{user.name}, {user.age}</p>
-              <p>{user.area} - {user.area_level}</p>
-              <p>{user.email}</p>
-              <p>Celular: {user.cellphone}</p>
-              <p>Endereço: {user.address}</p>
-              <p>{user.city} - {user.state}</p>
-            </div>
-            <div className="content">
-              <h2>Objetivo</h2>
-              <p> {user.goal} </p>
-            </div>
-              <div className="content">
-                <h2>Experiencia</h2>
-                {user.companyOccupation !== 'NOT_PRINT' ? <p> Cargo: {user.companyOccupation} </p> : <></>}
-                {user.companyStartEnd !== 'NOT_PRINT' ? <p> Período: {user.companyStartEnd} </p> : <></>}
-                <p>{user.companyName}</p>
-                {user.companyDescription !== 'NOT_PRINT' ? <p>{user.companyDescription}</p> : <></>}
-              </div> 
-            <div className="content">
-              <h2>Curso</h2>
-              <p>{user.courseName}</p>
-              <p>{user.courseSchool}</p>
-              <p>{user.courseEndYear}</p>
-            </div>
-            {(user.courses !== 'NOT_PRINT') ?
-              <div className="content">
-                <h2>Formação Complementar</h2>
-                <p>{user.courses}</p>
-              </div> : <></>}
-            {(user.linkedln_link !== 'NOT_PRINT') ?
-              <div className="content">
-                <h2>Midias Sociais</h2>
-                <p> <a href={user.linkedln_link} target='_blank' rel="noreferrer" >{user.linkedln_link}</a></p>
-              </div> : <></>}
-            <div className="content">
-              <h2>Feedback</h2>
-              <p>{user.feedback}</p>
-              <p>NOTA: {user.grade}</p>
-            </div>
-            <div className="content">
-              <h2>Criado em</h2>
-              <p>{user.created_at}</p>
-            </div>
-          </Form></> : <ReactLoading type='bars' color="black" height={'5vh'} width={'10vh'} />}
+        <p>Converse agora mesmo com o Curvi</p>
+        <p>Clicando na caixinha azul no canto inferior da tela</p>
+        <p>ou</p>
+        <p>Clique no botão e veja o seu Currículo</p>
+        <ChatBot/>
+        <Box>
+        <button><a  href='/resume'>Ver Currículo</a></button>
+        </Box>
       </header>
     </div>
   );
