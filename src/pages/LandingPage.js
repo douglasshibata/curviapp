5b7a94db0bc530db1dfe095ce4d6d8bd7b55b0e1
Douglas Shibata <douglas.shibata11@gmail.com>
2020-11-24 13:01:20 -0300

    Removendo visualização do cv na página inicial

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 import logo from '../assets/logo.gif';
 import ChatBot from './Chatbot/Chatbot';
-import { Box } from './style';
+//import { Box } from './style';
 
 function LandingPage() {
   return (
@@ -12,12 +12,10 @@ function LandingPage() {
         <p>Bem vindo a Curvi</p>
         <p>Converse agora mesmo com o Curvi</p>
         <p>Clicando na caixinha azul no canto inferior da tela</p>
-        <p>ou</p>
-        <p>Clique no botão e veja o seu Currículo</p>
         <ChatBot/>
-        <Box>
+        {/* <Box>
         <button><a  href='/resume'>Ver Currículo</a></button>
-        </Box>
+        </Box> */}
       </header>
     </div>
   );
