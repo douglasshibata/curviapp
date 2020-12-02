e61916b6c254dfe65e3b0b305e4f16dae021354c
Douglas Shibata <douglas.shibata11@gmail.com>
2020-12-01 21:14:43 -0300

    Change the text and the bot

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import '../App.css';
 import logo from '../assets/logo.gif';
-import ChatBot from './Chatbot/Chatbot';
+//import ChatBot from './Chatbot/Chatbot';
 //import { Box } from './style';
 
 function LandingPage() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Bem vindo ao Curvi</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bem vindo o Curvi</p>
-        <p>Converse agora mesmo com o Curvi</p>
-        <ChatBot/>
+        <h2>Você demora muito fazendo um currículo e não sabe o que colocar?</h2>
+        <p>
+          Converse agora mesmo com ele clicando no canto inferior da sua tela e faça seu currículo (rápido e fácil)!</p>
+        {/* <ChatBot /> */}
         {/* <Box>
         <button><a  href='/resume'>Ver Currículo</a></button>
         </Box> */}
