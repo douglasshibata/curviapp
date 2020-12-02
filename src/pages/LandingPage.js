7727951fddb5cfd5e125a1155b1cd9753a6e6914
Douglas Shibata <douglas.shibata11@gmail.com>
2020-12-02 11:33:08 -0300

    Mudando texto inicial

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import logo from '../assets/logo.gif';
+import logo from '../assets/bot.jpg';
 //import ChatBot from './Chatbot/Chatbot';
 //import { Box } from './style';
 
@@ -8,11 +8,11 @@ function LandingPage() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bem vindo ao Curvi</h1>
+        <h1>Bem-vindo ao Curvi</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Você demora muito fazendo um currículo e não sabe o que colocar?</h2>
+        <h2>Você demora muito fazendo seu currículo ou não sabe como faze-lo?</h2>
         <p>
-          Converse agora mesmo com ele clicando no canto inferior da sua tela e faça seu currículo (rápido e fácil)!</p>
+        Converse comigo clicando no canto inferior da sua tela e faça seu currículo de forma rápida e fácil!</p>
         {/* <ChatBot /> */}
         {/* <Box>
         <button><a  href='/resume'>Ver Currículo</a></button>
