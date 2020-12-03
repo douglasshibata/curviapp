4c6c29cfddd6936baac9a156dc5607f5b056ef34
Douglas Shibata <douglas.shibata11@gmail.com>
2020-12-03 16:16:06 -0300

    ALterando a página de erro

--- src/pages/Error/ErrorPage.js
+++ src/pages/Error/ErrorPage.js
@@ -7,8 +7,11 @@ function ErrorPage() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>PÁGINA NÃO ENCONTRADA</h1>
+        <h1>DESCULPE</h1>
+        <h2>PÁGINA NÃO ENCONTRADA</h2>
         <a href='/'>Ir para Home</a>
+        <p>Para mais informações ou dúvidas mande um email para <a href='mailto:curvistartup@gmail.com'>curvistartup@gmail.com</a></p>
+        {/* <p>Redes Sociais @curvistartup</p> */}
       </header>
     </div>
   );
